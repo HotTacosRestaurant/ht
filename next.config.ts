e06ea383c7611acb2ba22bd20ec45ee56bba07f2
nixinxcom <admin@nixinx.com>
2026-03-23 11:57:12 -0400

    VErsion publicable de Androd App

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const isNativeBuild = process.env.BUILD_TARGET === "native";
 const nextConfig: NextConfig = {
   ...(isNativeBuild ? { output: "export" } : {}),
   trailingSlash: isNativeBuild,
+  output: 'export',
   images: {
     unoptimized: isNativeBuild,
     remotePatterns: [
